f50f871f2a0a052e73f2f3c084c6bf3c0ad0e640
Dave Schmid <dave@pulpfree.org>
2019-07-22 03:10:57 -0500

    Updated to delete images
    Needed to delete the first minutes worth of images after midnight since they are Cacti errors (subtraction of time before/after midnight)

--- scripts/cacti/cron_network-tylegw-internet_fullday.sh
+++ scripts/cacti/cron_network-tylegw-internet_fullday.sh
@@ -10,4 +10,13 @@ DATEDAY=`date --date="$1 day ago" +%d`
 
 FULLPATH="${LOGPARENT}/${DATEYEAR}/${DATEMONTH}/${DATEDAY}"
 
-ffmpeg -r 60 -f image2 -pattern_type glob -i "${FULLPATH}/network-tylegw-internetfullday_*.png" -vcodec libx264 -crf 23 -pix_fmt yuv420p ${LOGPARENT}/network-tylegw-internetfullday_${DATEYEAR}${DATEMONTH}${DATEDAY}.mp4
+# Delete first minute's images first since they contain an error
+
+START=`/usr/bin/date -d "-$1 day 00:00:00"`
+END=`/usr/bin/date -d "-$1 day 00:01:00"`
+
+/usr/bin/find ${FULLPATH} -maxdepth 1 -type f -name "*.png" -newermt "${START}" -not -newermt "${END}" -delete
+
+/root/bin/ffmpeg -r 120 -f image2 -pattern_type glob -i "${FULLPATH}/network-tylegw-internetfullday_*.png" \
+  -vcodec libx264 -crf 23 -pix_fmt yuv420p -r 60 \
+  ${LOGPARENT}/network-tylegw-internetfullday_${DATEYEAR}${DATEMONTH}${DATEDAY}.mp4
