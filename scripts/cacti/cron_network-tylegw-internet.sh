5a835892d4c992113734aad9c1a8c8ad1f04a1ae
Dave Schmid <dave@pulpfree.org>
2019-07-22 03:11:34 -0500

    Formatting

--- scripts/cacti/cron_network-tylegw-internet.sh
+++ scripts/cacti/cron_network-tylegw-internet.sh
@@ -10,4 +10,5 @@ DATEDAY=`date --date="$1 day ago" +%d`
 
 FULLPATH="${LOGPARENT}/${DATEYEAR}/${DATEMONTH}/${DATEDAY}"
 
-ffmpeg -r 30 -f image2 -pattern_type glob -i "${FULLPATH}/network-tylegw-internet_*.png" -vcodec libx264 -crf 23 -pix_fmt yuv420p ${LOGPARENT}/network-tylegw-internet_${DATEYEAR}${DATEMONTH}${DATEDAY}.mp4
+ffmpeg -r 30 -f image2 -pattern_type glob -i "${FULLPATH}/network-tylegw-internet_*.png" -vcodec libx264 \
+-crf 23 -pix_fmt yuv420p ${LOGPARENT}/network-tylegw-internet_${DATEYEAR}${DATEMONTH}${DATEDAY}.mp4
