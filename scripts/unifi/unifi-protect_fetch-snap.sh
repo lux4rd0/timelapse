59416daedae63269553a5792fdcc1cdb5c8dbac0
Dave Schmid <dave@pulpfree.org>
2020-08-13 18:17:33 -0500

    Update unifi-protect_fetch-snap.sh

--- scripts/unifi/unifi-protect_fetch-snap.sh
+++ scripts/unifi/unifi-protect_fetch-snap.sh
@@ -4,7 +4,7 @@
 # Unifi Protect WGET Snap
 #
 
-# crontab UBNT Timelapse
+# crontab Unifi Protect Timelapse
 
 #*/1 * * * * /mnt/attic/scripts/timelapse/unifi/unifi-protect_fetch-snap.sh frontdoor >/dev/null 2>&1
 #*/1 * * * * /mnt/attic/scripts/timelapse/unifi/unifi-protect_fetch-snap.sh pergolanorth >/dev/null 2>&1
