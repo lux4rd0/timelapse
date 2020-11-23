23e21685a69eaddc60a14deaf8254f5071ac97d4
Dave Schmid <dave@pulpfree.org>
2020-11-23 13:23:35 -0600

    Create grafana-tl-createlist-darkstat01.sh

---
+++ scripts/grafana/grafana-tl-createlist-darkstat01.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# Generate Sequence of Images from Grafana for Timelapse
+# Darkstat
+#
+
+# Use "cat list.txt |parallel -j8" to pull images
+
+
+rm /mnt/attic/timelapse/grafana/external/grafana-darkstat.txt
+
+START_TIME=$1
+END_TIME=$2
+INTERVAL=$3
+PANEL_TIME_FRAME=$4
+
+WIDTH=$5
+HEIGHT=$6
+
+
+## Random Folder
+
+RANDOM_FOLDER=$(( $RANDOM ))
+
+while [ "${END_TIME}" -gt "${START_TIME}" ]
+do
+
+#echo "${START_TIME}"
+
+## Fetch Image
+
+
+IMAGE_PARENT="/mnt/attic/timelapse/grafana/external/darkstat"
+
+FULL_IMAGE_PATH="${IMAGE_PARENT}/${RANDOM_FOLDER}"
+
+    if [ ! -d "${FULL_IMAGE_PATH}" ]
+        then
+        mkdir -p "${FULL_IMAGE_PATH}"
+    fi
+
+TIME_FROM=`expr ${START_TIME} - ${PANEL_TIME_FRAME}`
+TIME_TO="${START_TIME}"
+
+#echo "Image: ${FULL_IMAGE_PATH} TIME_TO:${TIME_TO} TIME_FROM: ${TIME_FROM}"
+
+echo "${FULL_IMAGE_PATH}/${TIME_TO}.png|/usr/bin/docker container run -i --rm -v /mnt/attic:/mnt/attic -e TZ=America/Chicago docker01.tylephony.com:5000/lux4rd0/alpine-chrome:latest --headless --no-sandbox --enable-logging --force-device-scale-factor=2 --window-size=${WIDTH},${HEIGHT} --hide-scrollbars --run-all-compositor-stages-before-draw --virtual-time-budget=1000000 --screenshot=${FULL_IMAGE_PATH}/${TIME_TO}.png http://grafana.tylephony.com/d/lux4rd0-darkstat01/darkstat\?
+orgId=1\&from=${TIME_FROM}000\&to=${TIME_TO}000\&var-host=All\&var-Smooth=1m\&theme=dark\&kiosk" >> /mnt/attic/timelapse/grafana/external/grafana-darkstat.txt
+
+
+START_TIME=`expr ${START_TIME} + ${INTERVAL}`
+
+done
+
+tail -1 /mnt/attic/timelapse/grafana/external/grafana-darkstat.txt
